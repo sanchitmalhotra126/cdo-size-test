code-dot-org/code-dot-org

commit 2b0d05c9b3a39c71b7bb635cd3c0f4b4ea57d40c

Enable scrolling through blocks in flyout (pair with bbuchanan)

blockly-core/core/ui/block_space/flyout.js
@@ -407,7 +407,7 @@ Blockly.Flyout.prototype.show = function(xmlList) {
   };
 
   // Bind mousedown on the flyout background
-  this.blockSpace_.bindBeginPanDragHandler(this.svgBackground_);
+  this.blockSpace_.bindBeginPanDragHandler(this.svgGroup_);
 
   // Create the blocks to be shown in this flyout.
   var blocks = [];

blockly-core/core/ui/block_space/pan_drag_handler.js
@@ -219,21 +219,18 @@ Blockly.PanDragHandler.prototype.onPanDragTargetMouseDown_ = function (e) {
     this.onTargetMouseDown_();
   }
 
-  var isClickDirectlyOnDragTarget = e.target && e.target === this.target_;
-
   // Clicking on the flyout background clears the global selection
-  if (Blockly.selected && !Blockly.readOnly && isClickDirectlyOnDragTarget) {
+  if (Blockly.selected && !Blockly.readOnly) {
     Blockly.selected.unselect();
   }
 
   // On left-click on scrollable area, begin scroll-drag
   // In readonly mode, we scroll-drag when clicking through a block, too.
-  if (this.blockSpace_.scrollbarPair && !Blockly.isRightButton(e) &&
-      (Blockly.readOnly || isClickDirectlyOnDragTarget)) {
+  if (this.blockSpace_.scrollbarPair && !Blockly.isRightButton(e)) {
     this.beginDragScroll_(e);
 
     // Don't click through to the workspace drag handler, or the browser
-    // default drag/scroll handlers.
+    // default drag/scroll handlers
     e.stopPropagation();
     e.preventDefault();
   }

sanchitmalhotra126/cdo-size-test

commit 2a3294364209e4dd45ba18e84937d6ee160749b8

Fix contract editor click-into-input while retaining pan-drag-on-flyout behavior - Regressed in 2b0d05c9b3a39c71b7bb635cd3c0f4b4ea57d40c

blockly-core/core/ui/block_space/block_space.js
@@ -30,6 +30,7 @@ goog.provide('Blockly.BlockSpace');
 goog.require('Blockly.ScrollbarPair');
 goog.require('Blockly.Trashcan');
 goog.require('Blockly.PanDragHandler');
+goog.require('Blockly.ScrollOnWheelHandler');
 goog.require('Blockly.ScrollOnBlockDragHandler');
 goog.require('Blockly.Xml');
 goog.require('goog.array');
@@ -80,10 +81,15 @@ Blockly.BlockSpace = function(blockSpaceEditor, getMetrics, setMetrics) {
 
   /**
    * Encapsulates state used to make pan-drag work.
-   * @type {Blockly.PanDragHandler}
-   * @private
+   * @private {Blockly.PanDragHandler}
    */
   this.panDragHandler_ = new Blockly.PanDragHandler(this);
+
+  /**
+   * Encapsulates state used to make scroll-on-mousewheel work.
+   * @private {Blockly.ScrollOnWheelHandler}
+   */
+  this.scrollOnWheelHandler_ = new Blockly.ScrollOnWheelHandler(this);
 
   /**
    * Encapsulates state used to make scroll-on-block-drag work.
@@ -1053,6 +1059,14 @@ Blockly.BlockSpace.prototype.updateScrollableSize = function () {
 Blockly.BlockSpace.prototype.bindBeginPanDragHandler = function (target,
                                                                  onDragTargetMouseDown) {
   this.panDragHandler_.bindBeginPanDragHandler(target, onDragTargetMouseDown);
+};
+
+/**
+ * @param {!EventTarget} target - Element which initiates pan-drag mode when
+ *        clicked directly.
+ */
+Blockly.BlockSpace.prototype.bindScrollOnWheelHandler = function (target) {
+  this.scrollOnWheelHandler_.bindTo(target);
 };
 
 /**

blockly-core/core/ui/block_space/block_space_editor.js
@@ -445,6 +445,7 @@ Blockly.BlockSpaceEditor.prototype.init_ = function() {
   // Bind pan-drag handlers
   this.blockSpace.bindBeginPanDragHandler(this.svg_,
     goog.bind(this.hideChaff, this));
+  this.blockSpace.bindScrollOnWheelHandler(this.svg_);
 
   Blockly.bindEvent_(Blockly.WidgetDiv.DIV, 'contextmenu', null,
     Blockly.blockContextMenu);

blockly-core/core/ui/block_space/flyout.js
@@ -406,8 +406,10 @@ Blockly.Flyout.prototype.show = function(xmlList) {
     y: margin
   };
 
-  // Bind mousedown on the flyout background
-  this.blockSpace_.bindBeginPanDragHandler(this.svgGroup_);
+  // We bind mouse pan-drag on the background group so blocks are ignored.
+  this.blockSpace_.bindBeginPanDragHandler(this.svgBackground_);
+  // We bind scrollwheel on the full group so wheeling over blocks works.
+  this.blockSpace_.bindScrollOnWheelHandler(this.svgGroup_);
 
   // Create the blocks to be shown in this flyout.
   var blocks = [];

blockly-core/core/ui/block_space/pan_drag_handler.js
@@ -18,8 +18,7 @@
  */
 
 /**
- * @fileoverview Panning (click-drag and mousewheel) scroll interaction handler
- * @author fraser@google.com (Neil Fraser)
+ * @fileoverview Click-drag panning interaction handler for BlockSpaces
  */
 'use strict';
 
@@ -31,82 +30,69 @@ goog.provide('Blockly.PanDragHandler');
  */
 Blockly.PanDragHandler = function (blockSpace) {
   /**
-   * @type {Blockly.BlockSpace}
-   * @private
+   * @private {Blockly.BlockSpace}
    */
   this.blockSpace_ = blockSpace;
 
   /**
    * Element which initiates pan-drag mode when clicked directly.
-   * @type {EventTarget}
-   * @private
+   * @private {EventTarget}
    */
   this.target_ = null;
 
   /**
-   * @type {function}
-   * @private
+   * @private {function}
    */
   this.onTargetMouseDown_ = null;
 
   /**
-   * @type {BindData}
-   * @private
+   * @private {BindData}
    */
   this.mouseDownEventBindData_ = null;
 
   /**
-   * @type {BindData}
-   * @private
+   * @private {BindData}
    */
   this.wheelEventBindData_ = null;
 
   /**
    * (for Safari)
-   * @type {BindData}
-   * @private
+   * @private {BindData}
    */
   this.mousewheelEventBindData_ = null;
 
   /**
-   * @type {BindData}
-   * @private
+   * @private {BindData}
    */
   this.contextMenuBlockEventBindData_ = null;
 
   /**
-   * @type {BindData}
-   * @private
+   * @private {BindData}
    */
   this.mouseMoveEventBindData_ = null;
 
   /**
-   * @type {BindData}
-   * @private
+   * @private {BindData}
    */
   this.mouseUpEventBindData_ = null;
 
   /**
-   * @type {number}
-   * @private
+   * @private {number}
    */
   this.startMouseX_ = null;
 
   /**
-   * @type {number}
-   * @private
+   * @private {number}
    */
   this.startMouseY_ = null;
 
   /**
-   * @type {number}
-   * @private
+   * @private {number}
    */
   this.startScrollX_ = null;
 
   /**
-   * @type {number}
-   * @private
+   * @private {number}
    */
   this.startScrollY_ = null;
 };
@@ -127,12 +113,6 @@ Blockly.PanDragHandler.prototype.bindBeginPanDragHandler = function (target,
   this.mouseDownEventBindData_ = Blockly.bindEvent_(
       target, 'mousedown', this, this.onPanDragTargetMouseDown_);
 
-  this.wheelEventBindData_ = Blockly.bindEvent_(target, 'wheel', this, this.onWheel_);
-
-  // Safari uses 'mousewheel'
-  this.mousewheelEventBindData_ = Blockly.bindEvent_(
-      target, 'mousewheel', this, this.onWheel_);
-
   // Also block the context menu on the pan-drag target element
   this.contextMenuBlockEventBindData_ = Blockly.bindEvent_(
       target, 'contextmenu', null, Blockly.blockContextMenu);
@@ -146,16 +126,6 @@ Blockly.PanDragHandler.prototype.unbindBeginPanDragHandler = function () {
   if (this.mouseDownEventBindData_) {
     Blockly.unbindEvent_(this.mouseDownEventBindData_);
     this.mouseDownEventBindData_ = null;
-  }
-
-  if (this.wheelEventBindData_) {
-    Blockly.unbindEvent_(this.wheelEventBindData_);
-    this.wheelEventBindData_ = null;
-  }
-
-  if (this.mousewheelEventBindData_) {
-    Blockly.unbindEvent_(this.mousewheelEventBindData_);
-    this.mousewheelEventBindData_ = null;
   }
 
   if (this.contextMenuBlockEventBindData_) {
@@ -212,42 +182,21 @@ Blockly.PanDragHandler.prototype.onPanDragTargetMouseDown_ = function (e) {
     this.onTargetMouseDown_();
   }
 
+  var isClickDirectlyOnDragTarget = e.target && e.target === this.target_;
+
   // Clicking on the flyout background clears the global selection
-  if (Blockly.selected && !Blockly.readOnly) {
+  if (Blockly.selected && !Blockly.readOnly && isClickDirectlyOnDragTarget) {
     Blockly.selected.unselect();
   }
 
   // On left-click on scrollable area, begin scroll-drag
   // In readonly mode, we scroll-drag when clicking through a block, too.
-  if (this.blockSpace_.scrollbarPair && !Blockly.isRightButton(e)) {
+  if (this.blockSpace_.scrollbarPair && !Blockly.isRightButton(e) &&
+      (isClickDirectlyOnDragTarget || Blockly.readOnly)) {
     this.beginDragScroll_(e);
 
     // Don't click through to the workspace drag handler, or the browser
     // default drag/scroll handlers
-    e.stopPropagation();
-    e.preventDefault();
-  }
-};
-
-/**
- * Scroll the blockspace up or down based on wheel scrolling.
- * @param {!Event} e Mouse wheel scroll event.
- * @private
- */
-Blockly.PanDragHandler.prototype.onWheel_ = function(e) {
-  if (!this.blockSpace_.scrollbarPair) {
-    return;
-  }
-
-  // + is down
-  var wheelDelta = Blockly.getNormalizedWheelDeltaY(e);
-  if (wheelDelta) {
-    this.blockSpace_.scrollWithAnySelectedBlock(
-      this.blockSpace_.getScrollOffsetX(),
-      this.blockSpace_.getScrollOffsetY() + wheelDelta,
-      e.clientX, e.clientY);
-
-    // Don't scroll the page.
     e.stopPropagation();
     e.preventDefault();
   }

blockly-core/core/ui/block_space/scroll_on_wheel_handler.js
@@ -1,0 +1,105 @@
+/**
+ * Visual Blocks Editor
+ *
+ * Copyright 2011 Google Inc.
+ * http://blockly.googlecode.com/
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * @fileoverview scroll-blockspace-on-mousewheel interaction handler
+ */
+'use strict';
+
+goog.provide('Blockly.ScrollOnWheelHandler');
+
+/**
+ * @param {!Blockly.BlockSpace} blockSpace
+ * @constructor
+ */
+Blockly.ScrollOnWheelHandler = function (blockSpace) {
+  /**
+   * @type {Blockly.BlockSpace}
+   * @private
+   */
+  this.blockSpace_ = blockSpace;
+
+  /**
+   * @type {BindData}
+   * @private
+   */
+  this.wheelEventBindData_ = null;
+
+  /**
+   * (for Safari)
+   * @type {BindData}
+   * @private
+   */
+  this.mousewheelEventBindData_ = null;
+};
+
+/**
+ * Establish a mousewheel handler on the given wheelTarget that will
+ * @param {!EventTarget} wheelTarget
+ */
+Blockly.ScrollOnWheelHandler.prototype.bindTo = function (wheelTarget) {
+  this.unbindWheelScrollHandler();
+
+  this.wheelEventBindData_ = Blockly.bindEvent_(wheelTarget,
+      'wheel', this, this.onWheel_);
+
+  // Safari uses 'mousewheel'
+  this.mousewheelEventBindData_ = Blockly.bindEvent_(
+      wheelTarget, 'mousewheel', this, this.onWheel_);
+};
+
+/**
+ * Unbinds previously bound handler to begin pan-drag.  Safe to call if no
+ * such handler is bound.
+ */
+Blockly.ScrollOnWheelHandler.prototype.unbindWheelScrollHandler = function () {
+  if (this.wheelEventBindData_) {
+    Blockly.unbindEvent_(this.wheelEventBindData_);
+    this.wheelEventBindData_ = null;
+  }
+
+  if (this.mousewheelEventBindData_) {
+    Blockly.unbindEvent_(this.mousewheelEventBindData_);
+    this.mousewheelEventBindData_ = null;
+  }
+};
+
+/**
+ * Scroll the BlockSpace up or down based on wheel scrolling.
+ * @param {!Event} e Mouse wheel scroll event.
+ * @private
+ */
+Blockly.ScrollOnWheelHandler.prototype.onWheel_ = function(e) {
+  if (!this.blockSpace_.scrollbarPair) {
+    return;
+  }
+
+  // + is down
+  var wheelDelta = Blockly.getNormalizedWheelDeltaY(e);
+  if (wheelDelta) {
+    this.blockSpace_.scrollWithAnySelectedBlock(
+      this.blockSpace_.getScrollOffsetX(),
+      this.blockSpace_.getScrollOffsetY() + wheelDelta,
+      e.clientX, e.clientY);
+
+    // Don't scroll the page.
+    e.stopPropagation();
+    e.preventDefault();
+  }
+};

blockly-core/core/ui/function_editor.js
@@ -445,6 +445,7 @@ Blockly.FunctionEditor.prototype.create_ = function() {
   this.modalBlockSpace.bindBeginPanDragHandler(this.container_, goog.bind(function () {
     this.modalBlockSpaceEditor.hideChaff();
   }, this));
+  this.modalBlockSpace.bindScrollOnWheelHandler(this.container_);
 
   Blockly.modalBlockSpace = this.modalBlockSpace;
   Blockly.modalBlockSpaceEditor = this.modalBlockSpaceEditor;

blockly-core/tests/test_dependency_map.js
@@ -1302,12 +1302,13 @@ goog.addDependency('../../../core/namespaces/blocks.js', ['Blockly.Blocks'], [])
 goog.addDependency('../../../core/namespaces/msg.js', ['Blockly.Msg'], []);
 goog.addDependency('../../../core/ui/block.js', ['Blockly.Block'], ['Blockly.BlockSpace', 'Blockly.BlockSvg', 'Blockly.BlockSvgFramed', 'Blockly.BlockSvgFunctional', 'Blockly.Blocks', 'Blockly.Comment', 'Blockly.Connection', 'Blockly.ContextMenu', 'Blockly.Input', 'Blockly.Msg', 'Blockly.Mutator', 'Blockly.Warning', 'Blockly.Xml', 'goog.Timer', 'goog.asserts', 'goog.string']);
 goog.addDependency('../../../core/ui/block_space/auto_scroll.js', ['Blockly.AutoScroll'], ['goog.async.AnimationDelay']);
-goog.addDependency('../../../core/ui/block_space/block_space.js', ['Blockly.BlockSpace'], ['Blockly.PanDragHandler', 'Blockly.ScrollOnBlockDragHandler', 'Blockly.ScrollbarPair', 'Blockly.Trashcan', 'Blockly.Xml', 'goog.array', 'goog.math', 'goog.math.Coordinate']);
+goog.addDependency('../../../core/ui/block_space/block_space.js', ['Blockly.BlockSpace'], ['Blockly.PanDragHandler', 'Blockly.ScrollOnBlockDragHandler', 'Blockly.ScrollOnWheelHandler', 'Blockly.ScrollbarPair', 'Blockly.Trashcan', 'Blockly.Xml', 'goog.array', 'goog.math', 'goog.math.Coordinate']);
 goog.addDependency('../../../core/ui/block_space/block_space_editor.js', ['Blockly.BlockSpaceEditor'], ['Blockly.BlockSpace', 'goog.array', 'goog.style']);
 goog.addDependency('../../../core/ui/block_space/flyout.js', ['Blockly.Flyout'], ['Blockly.Block', 'Blockly.Comment', 'goog.math.Rect']);
 goog.addDependency('../../../core/ui/block_space/horizontal_flyout.js', ['Blockly.HorizontalFlyout'], ['Blockly.Block', 'Blockly.Flyout']);
 goog.addDependency('../../../core/ui/block_space/pan_drag_handler.js', ['Blockly.PanDragHandler'], []);
 goog.addDependency('../../../core/ui/block_space/scroll_on_block_drag_handler.js', ['Blockly.ScrollOnBlockDragHandler'], ['Blockly.AutoScroll', 'goog.math.Vec2']);
+goog.addDependency('../../../core/ui/block_space/scroll_on_wheel_handler.js', ['Blockly.ScrollOnWheelHandler'], []);
 goog.addDependency('../../../core/ui/block_space/scrollbar.js', ['Blockly.Scrollbar', 'Blockly.ScrollbarPair'], []);
 goog.addDependency('../../../core/ui/block_space/toolbox.js', ['Blockly.Toolbox'], ['Blockly.Flyout', 'goog.events.BrowserFeature', 'goog.math.Rect', 'goog.style', 'goog.ui.tree.TreeControl', 'goog.ui.tree.TreeNode']);
 goog.addDependency('../../../core/ui/block_space/trashcan.js', ['Blockly.Trashcan'], ['goog.math.Rect']);