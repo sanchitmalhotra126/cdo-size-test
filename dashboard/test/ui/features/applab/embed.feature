mwatt/code-dot-org

Beginning of an eyes test

dashboard/test/ui/features/applab/embed.feature
@@ -1,0 +1,16 @@
+@dashboard_db_access
+@as_student
+Feature: App Lab Embed
+
+  Background:
+    Given I start a new Applab project
+
+  Scenario: App Lab Embed
+    Given I switch to text mode
+    And I append text to droplet "button('hello', 'world');"
+    And I press "runButton"
+    And I wait until element "#divApplab > .screen > button#hello" is visible
+    And element "#divApplab > .screen > button#hello" contains text "world"
+    And I press "resetButton"
+
+    When I navigate to the embedded version of my project

sanchitmalhotra126/cdo-size-test

dashboard/test/ui/features/applab/embed.feature
@@ -1,0 +1,17 @@
+@dashboard_db_access
+@as_student
+Feature: App Lab Embed
+
+  Background:
+    Given I start a new Applab project
+
+  Scenario: App Lab Embed
+    Given I switch to text mode
+    And I append text to droplet "button('hello', 'world');"
+    And I press "runButton"
+    And I wait until element "#divApplab > .screen > button#hello" is visible
+    And element "#divApplab > .screen > button#hello" contains text "world"
+    And I press "resetButton"
+
+    Then I navigate to the embedded version of my project
+    And I wait until element "#divApplab > .screen > button#hello" is visible within element "iframe"